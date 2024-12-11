4488b530672e1f647d2dad21f06ec20c866bc928
GeloDGreat <carions46@gmail.com>
2024-12-11 08:25:47 +0800

    Refactor Projects component to enhance project details and support multiple live links for each project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,68 +5,143 @@ import { ExternalLink, Github } from 'lucide-react';
 
 const projects = [
 	{
-		title: 'FTBLRLife for Android',
+		title: 'FTBLRLife',
 		description: 'Sports mobile applications that provides opportunities for talented people in the sports industry',
 		image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'Redux', 'GraphQL', 'Typescript'],
-		live: 'https://play.google.com/store/apps/details?id=com.footballer.app',
+		live: [
+			{
+				title: 'FTBLRLife for Android',
+				url: 'https://play.google.com/store/apps/details?id=com.footballer.app',
+			},
+			{
+				title: 'FTBLRLife for iOS',
+				url: 'https://apps.apple.com/ph/app/ftblrlife/id6444323230',
+			},
+		],
 	},
 	{
-		title: 'FTBLRLife for iOS',
-		description: 'Sports mobile applications that provides opportunities for talented people in the sports industry',
-		image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&w=800&q=80',
-		tech: ['React', 'Firebase', 'React Native', 'Redux', 'GraphQL', 'Typescript'],
-		live: 'https://apps.apple.com/ph/app/ftblrlife/id6444323230',
-	},
-	{
-		title: 'Glassons Android',
-		description: 'An e-commerce mobile application for Glassons on Android',
+		title: 'Glassons',
+		description: 'An e-commerce mobile application for Glassons',
 		image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'AWS Amplify', 'GraphQL', 'Typescript'],
-		live: 'https://play.google.com/store/apps/details?id=com.glassons.customer.app&hl=en',
+		live: [
+			{
+				title: 'Glassons Android',
+				url: 'https://play.google.com/store/apps/details?id=com.glassons.customer.app&hl=en',
+			},
+			{
+				title: 'Glassons iOS',
+				url: 'https://apps.apple.com/nz/app/glassons/id1525273016',
+			},
+		],
 	},
 	{
-		title: 'Glassons iOS',
-		description: 'An e-commerce mobile application for Glassons on iOS',
+		title: 'Hallensteins',
+		description: 'An e-commerce mobile application for Hallensteins',
 		image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'AWS Amplify', 'GraphQL', 'Typescript'],
-		live: 'https://apps.apple.com/nz/app/glassons/id1525273016',
+		live: [
+			{
+				title: 'Hallensteins Android',
+				url: 'https://play.google.com/store/apps/details?id=com.hallensteins.customer.app&hl=en',
+			},
+			{
+				title: 'Hallensteins iOS',
+				url: 'https://apps.apple.com/nz/app/hallensteins/id6453358386',
+			},
+		],
 	},
 	{
 		title: 'RedRat Android',
 		description: 'An e-commerce mobile application for RedRat on Android',
 		image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'AWS Amplify', 'GraphQL', 'Typescript'],
-		live: 'https://play.google.com/store/apps/details?id=nz.co.redrat.app&hl=en',
-	},
-	{
-		title: 'RedRat iOS',
-		description: 'An e-commerce mobile application for RedRat on iOS',
-		image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80',
-		tech: ['React', 'Firebase', 'React Native', 'AWS Amplify', 'GraphQL', 'Typescript'],
-		live: 'https://apps.apple.com/nz/app/red-rat/id1551890750',
+		live: [
+			{
+				title: 'RedRat Android',
+				url: 'https://play.google.com/store/apps/details?id=nz.co.redrat.app&hl=en',
+			},
+			{
+				title: 'RedRat iOS',
+				url: 'https://apps.apple.com/nz/app/red-rat/id1551890750',
+			},
+		],
 	},
 	{
 		title: 'Urban.com.au',
 		description: 'A real estate application for Urban.com.au on iOS',
-		image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80',
+		image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'Redux', 'Typescript'],
-		live: 'https://www.urban.com.au/',
+		live: [
+			{
+				title: 'Urban.com.au',
+				url: 'https://www.urban.com.au/',
+			},
+		],
 	},
 	{
 		title: 'Taply Mobile App',
 		description:
 			'An eCommerce mobile application that is integrated with Shopify and built using React Native, with more than 2000 clients and over 300 applications deployed on the Playstore and AppStore, Also this manages an automation deployment of the app from creating and listing the app upto releasing to the store.',
-		image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&w=800&q=80',
+		image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&w=800&q=80',
+
 		tech: ['React', 'Firebase', 'React Native', 'Redux', 'Typescript'],
-		live: 'https://play.google.com/store/apps/details?id=io.taply.initial',
+		live: [
+			{
+				title: 'Taply',
+				url: 'https://play.google.com/store/apps/details?id=io.taply.initial',
+			},
+		],
 	},
 	{
-		title: 'ShoreTrade and SFM Blue Mobile Applications',
+		title: 'SFM Blue Mobile Applications',
 		description: 'An eCommerce mobile app for one of the biggest fishery markets in Australia that helps local vendors buy and sell their goods with various and complex features.',
 		image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&w=800&q=80',
 		tech: ['React', 'Firebase', 'React Native', 'Redux', 'Typescript'],
-		live: 'https://apps.apple.com/us/app/sfm-blue-seller/id1623529645',
+		live: [
+			{
+				url: 'https://apps.apple.com/us/app/sfmblue-seller/id1623529645',
+				title: 'SFM Blue Seller iOS',
+			},
+			{
+				url: 'https://apps.apple.com/us/app/sfmblue-buyer/id1623528874',
+				title: 'SFM Blue Buyer iOS',
+			},
+			{
+				url: 'https://play.google.com/store/apps/details?id=com.shoretradeapp.sfmseller&hl=en&gl=US',
+				title: 'SFM Blue Seller Android',
+			},
+			{
+				url: 'https://play.google.com/store/apps/details?id=com.shoretradeapp.sfmbuyer&hl=en&gl=US',
+				title: 'SFM Blue Buyer Android',
+			},
+		],
+	},
+
+	{
+		title: 'ShoreTrade Mobile Applications',
+		description: 'An eCommerce mobile app for one of the biggest fishery markets in Australia that helps local vendors buy and sell their goods with various and complex features.',
+		image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&w=800&q=80',
+		tech: ['React', 'Firebase', 'React Native', 'Redux', 'Typescript'],
+		live: [
+			{
+				url: 'https://apps.apple.com/us/app/shoretrade-buyer/id1460087124',
+				title: 'ShoreTrade Seller iOS',
+			},
+			{
+				url: 'https://apps.apple.com/ph/app/shoretrade-seller/id1460087449',
+				title: 'ShoreTrade Buyer iOS',
+			},
+			{
+				url: 'https://play.google.com/store/apps/details?id=com.shoretradeapp.seller&hl=en_US&gl=US',
+				title: 'ShoreTrade Seller Android',
+			},
+			{
+				url: 'https://play.google.com/store/apps/details?id=com.shoretradeapp.buyer&hl=en_US&gl=US',
+				title: 'ShoreTrade Buyer Android',
+			},
+		],
 	},
 ];
 
@@ -102,11 +177,13 @@ export const Projects = () => {
 											<Github className='w-6 h-6' />
 										</a>
 									)}
-									{project.live && (
-										<a href={project.live} className='text-gray-300 hover:text-emerald-400 transition-colors'>
-											<ExternalLink className='w-6 h-6' />
-										</a>
-									)}
+
+									{project.live &&
+										project.live.map((link, linkIndex) => (
+											<a key={linkIndex} href={link.url} className='text-gray-300 hover:text-emerald-400 transition-colors'>
+												<span className='underline'>{link.title}</span>
+											</a>
+										))}
 								</div>
 							</div>
 						</motion.div>
